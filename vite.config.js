f1a07cfefedd966fce7ddb993c41bb2a765ef0cb
HalaszPeter1 <halasz.peter1@students.jedlik.eu>
2023-04-25 23:26:30 +0200

    asd

--- vite.config.js
+++ vite.config.js
@@ -10,14 +10,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server:{
-    proxy:{
-      '/api': {
-        target: 'https://sore-blue-elephant-toga.cyclic.app',
-        changeOrigin: true,
-        secure: true
-      }
-    }
   }
 })

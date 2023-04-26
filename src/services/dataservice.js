6088241eef0011abd11efe20cac7bd44ceb21211
HalaszPeter1 <halasz.peter1@students.jedlik.eu>
2023-04-26 09:30:40 +0200

    asdasdasdasdasdasdasdsasdasdadadasdasdadad

--- src/services/dataservice.js
+++ src/services/dataservice.js
@@ -4,6 +4,7 @@ const instance = Axios.create({
     baseURL: 'https://sore-blue-elephant-toga.cyclic.app',
     headers:{
         'Content-Type' : 'application/json',
+        'Access-Control-Allow-Origin': 'https://visionary-croquembouche-50b9b6.netlify.app/'
     },
     withCredentials:true
 });
